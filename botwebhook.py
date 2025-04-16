7e2200e9744d971c2b7ff773852a3b204f15065e
Ddott4 <sisarevtihon@gmail.com>
2025-04-16 23:38:39 +0300

    Update botwebhook.py

--- botwebhook.py
+++ botwebhook.py
@@ -131,10 +131,13 @@ app.on_startup.append(lambda app: on_startup(bot))
 app.on_shutdown.append(lambda app: on_shutdown(bot))
 
 if __name__ == "__main__":
-    web.run_app(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
-    async def handle_root(request):
+    # ==== ОБРАБОТКА ГЛАВНОЙ СТРАНИЦЫ ДЛЯ UPTIMEROBOT ====
+async def handle_root(request):
     return web.Response(text="✅ Bot is alive!")
 
 app.router.add_get("/", handle_root)
+
+    web.run_app(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+   
 
 
